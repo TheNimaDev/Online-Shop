fdc88608757ae236da975330690bc94a04bb8451
TheNimaDev <TheNimaDev@gmail.com>
2026-05-03 18:57:13 +0330

    Add And Config Some Packages Like dotenv

--- src/app.js
+++ src/app.js
@@ -2,6 +2,28 @@ const express = require("express")
 
 const app = express()
 
+const cors = require("cors")
+const helmet = require("helmet")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+
+
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(cors())
+app.use(session({
+    saveUninitialized: true,
+    resave: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        secure: false
+    }
+}))
+app.use(helmet());
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
 app.get("/", (req, res) => {
     res.send("ok")
 })
