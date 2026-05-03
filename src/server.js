41a36e6f88fd92ad5ef045be00ad5bf93dbaeca1
TheNimaDev <TheNimaDev@gmail.com>
2026-05-03 18:42:15 +0330

    Add src/server.js And src/app.js Files As Initial

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const http = require("http")
+const app = require("./app")
+
+const server = http.createServer(app)
+const port = 5000
+
+server.listen(port, () => {
+    console.log(`App run on port ${port}`)
+})
