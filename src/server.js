459fb31d1157eb617a38c5a473b5c4d44447774b
imNima10 <x1nima1x@gmail.com>
2026-05-03 18:57:13 +0330

    Add And Config Some Packages Like dotenv

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require("http")
 const app = require("./app")
 
 const server = http.createServer(app)
-const port = 5000
+const port = process.env.PORT
 
 server.listen(port, () => {
     console.log(`App run on port ${port}`)
