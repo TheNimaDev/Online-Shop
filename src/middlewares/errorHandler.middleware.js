0989eada18eb66772de1fba97b0443a610085f1f
imNima10 <x1nima1x@gmail.com>
2026-05-04 11:38:34 +0330

    Add middlewares/errorHandler.middleware File

---
+++ src/middlewares/errorHandler.middleware.js
@@ -0,0 +1,7 @@
+module.exports = async (err, req, res, next) => {
+    let status = err?.status
+    if (!status || isNaN(+status) || status > 511 || status < 200) status = 500
+    res.status(status).json({
+        message: err?.message ?? "InternalServerError"
+    })
+}
