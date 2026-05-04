c3cb3c2fe6195c98900987a9cccc894d6aaa2ba3
imNima10 <x1nima1x@gmail.com>
2026-05-04 11:36:10 +0330

    Add auth/register Route And router, controller, service, Repository Files

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser")
 const session = require("express-session")
 
 const config = require("./core/config")
+const authRouter = require("./routers/auth.route")
 
 
 app.use(cookieParser(config.getAppConfig().cookie_secret))
@@ -24,6 +25,7 @@ app.use(helmet());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use("/auth", authRouter)
 
 app.get("/", (req, res) => {
     res.send("ok")
