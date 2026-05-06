d144a16f2b0d08dcf345b196228012ddcd7a4176
imNima10 <x1nima1x@gmail.com>
2026-05-06 13:37:19 +0330

    Add user/change/password And user/change/info Routes And User router, controller, service, repository Files

---
+++ src/routers/user.route.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const userController = require("../controllers/user.controller")
+const checkAuthMiddleware = require("../middlewares/checkAuth.middleware")
+const authenticationGuard = require("../guards/authentication.guard")
+
+const router = express.Router()
+
+router.use(checkAuthMiddleware)
+router.use(authenticationGuard())
+
+router.post("/change/password", userController.changePassword)
+router.post("/change/info", userController.changeInfo)
+
+module.exports = router
