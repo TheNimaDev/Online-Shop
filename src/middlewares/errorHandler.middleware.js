6045579a7fc7c5b46d275529663fbd2c451f394b
TheNimaDev <TheNimaDev@gmail.com>
2026-05-08 14:22:02 +0330

    Refactor src/middlewares/errorHandler.middleware And src/middlewares/validator.middleware Files Logic

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,9 +1,11 @@
 module.exports = async (err, req, res, next) => {
-    let status = err?.status
-    if (!status || isNaN(+status) || status > 511 || status < 200) status = 500
-    res.status(status).json({
-        message: err?.message ?? "InternalServerError"
-    })
-    console.log(err);
-    
+    if (err.message.title == "validationError") {
+        res.status(err.status).send(err.message)
+    } else {
+        let status = err?.status
+        if (!status || isNaN(+status) || status > 511 || status < 200) status = 500
+        res.status(status).json({
+            message: err?.message ?? "InternalServerError"
+        })
+    }
 }
