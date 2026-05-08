00c6a90733afd9e5e25c5cf9a959a374fb0a275c
TheNimaDev <TheNimaDev@gmail.com>
2026-05-08 14:38:43 +0330

    Add User Routes Validators

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -3,25 +3,43 @@ const express = require("express")
 const userController = require("../controllers/user.controller")
 const checkAuthMiddleware = require("../middlewares/checkAuth.middleware")
 const authenticationGuard = require("../guards/authentication.guard")
+const validatorMiddleware = require("../middlewares/validator.middleware")
+const userValidator = require("../validator/user.validator")
 
 const router = express.Router()
 
 router.use(checkAuthMiddleware)
 router.use(authenticationGuard())
 
-router.post("/change/password", userController.changePassword)
-router.post("/change/info", userController.changeInfo)
+router.post("/change/password",
+    userValidator.changePassword(),
+    validatorMiddleware.validate().bind(validatorMiddleware),
+    userController.changePassword
+)
+router.post("/change/info",
+    userValidator.changeInfo(),
+    validatorMiddleware.validate().bind(validatorMiddleware),
+    userController.changeInfo
+)
 
 router.get("/favorites", userController.getFavorites)
 router.post("/favorites/delete/:productId", userController.deleteFavorite)
 router.post("/favorites/create/:productId", userController.createFavorite)
 
-router.post("/comment/:productId",userController.createComment)
+router.post("/comment/:productId",
+    userValidator.createComment(),
+    validatorMiddleware.validate().bind(validatorMiddleware),
+    userController.createComment
+)
 
-router.post("/note/create/:productId",userController.createNote)
-router.post("/note/update/:productId",userController.updateNote)
-router.post("/note/delete/:productId",userController.deleteNote)
-router.get("/note/:productId",userController.getNote)
-router.get("/notes",userController.getNotes)
+router.post("/note/create/:productId",
+    userValidator.createNote(),
+    validatorMiddleware.validate().bind(validatorMiddleware),
+    userController.createNote
+)
+router.post("/note/update/:productId", userController.updateNote)
+router.post("/note/delete/:productId", userController.deleteNote)
+router.get("/note/:productId", userController.getNote)
+router.get("/notes", userController.getNotes)
 
 module.exports = router
