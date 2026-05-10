fd0bb593313b631e2acb3f3f724a517b28a3194a
imNima10 <x1nima1x@gmail.com>
2026-05-10 13:22:43 +0330

    Add OrderItem Model And Migration Files And Reletions Between OrderItem And Order And Product Models

---
+++ src/sequelize/migrations/20260510094756-define-relationships-between-order-item-and-product-models.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    
+    
+    await queryInterface.addConstraint(
+      "tbl_orderItems",
+      {
+        fields: ["product_id"],
+        type: "FOREIGN KEY",
+        name: "fk_orderItem_product",
+        references: {
+          table: "tbl_products",
+          field: "id"
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
+      }
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint("tbl_orderItems", "fk_orderItem_product")
+  },
+}
