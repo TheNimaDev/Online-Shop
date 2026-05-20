9d5862313b1c33f2f228968860d5baf4eda82c94
TheNimaDev <TheNimaDev@gmail.com>
2026-05-20 17:27:54 +0330

    Add User Routes Documents (Comment And Note)

--- src/docs/user.doc.js
+++ src/docs/user.doc.js
@@ -1,6 +1,6 @@
 /**
  * @swagger
- * 
+ *
  * tags:
  *  name: User
  *  description: User Routes
@@ -18,26 +18,26 @@
  *              -   current_password
  *              -   new_password
  *          properties:
- *              current_password: 
+ *              current_password:
  *                  type: string
  *                  example: myCurrentPassword
- *              new_password: 
+ *              new_password:
  *                  type: string
  *                  example: myNewPassword
- * 
+ *
  *      changeInfo:
  *          type: object
  *          required:
  *              -   name
  *          properties:
- *              name: 
+ *              name:
  *                  type: string
  *                  example: Nima
  */
 
 /**
  * @swagger
- * 
+ *
  * /user/change/password:
  *  post:
  *      summary: Change Password With (current_password, new_password)
@@ -58,7 +58,7 @@
  *              description: Unauthorized Error (Authentication Error Or Password Is Incorrect)
  *          409:
  *              description: Conflict Error (New Password Is Match With Current Password)
- * 
+ *
  * /user/change/info:
  *  post:
  *      summary: Change Info With (Name)
@@ -77,7 +77,7 @@
  *              description: Success
  *          401:
  *              description: Unauthorized Error (Authentication Error)
- * 
+ *
  */
 
 
@@ -94,14 +94,14 @@
  *              description: Success
  *          401:
  *              description: Unauthorized Error (Authentication Error)
- * 
+ *
  * /user/favorites/delete/{productId}:
  *  post:
  *      summary: Delete Favorite With (productId)
  *      tags:
  *          -   User
  *      parameters:
- *          -   in: path        
+ *          -   in: path
  *              name: productId
  *              type: string
  *      responses:
@@ -111,14 +111,14 @@
  *              description: Unauthorized Error (Authentication Error)
  *          404:
  *              description: NotFound Error (Favorite Not Found)
- * 
+ *
  * /user/favorites/create/{productId}:
  *  post:
  *      summary: Create Favorite With (productId)
  *      tags:
  *          -   User
  *      parameters:
- *          -   in: path        
+ *          -   in: path
  *              name: productId
  *              type: string
  *      responses:
@@ -130,5 +130,182 @@
  *              description: NotFound Error (Favorite Not Found)
  *          409:
  *              description: Conflict Error (Product Already Is Favorite)
+ */
+
+
+// ?----------------- Comment Routes
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      createComment:
+ *          type: object
+ *          required:
+ *              -   text
+ *              -   positivePoints
+ *              -   negetivePoints
+ *              -   rate
+ *          properties:
+ *              text:
+ *                  type: string
+ *                  example: "its perfect"
+ *              positivePoints:
+ *                  type: object
+ *              negetivePoints:
+ *                  type: object
+ *              rate:
+ *                  type: number
+ *                  example: 5
+ */
+
+/**
+ * @swagger
+ * /user/comment/{productId}:
+ *  post:
+ *      summary: Create Comment With (productId, Text, PositivePoints, NegetivePoints, Rate)
+ *      tags:
+ *          -   User
+ *      parameters:
+ *          -   in: path        
+ *              name: productId
+ *              type: string
+ *      requestBody:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/createComment"
+ *      responses:
+ *          201:
+ *              description: Success
+ *          401:
+ *              description: Unauthorized Error (Authentication Error)
+ *          404:
+ *              description: NotFound Error (Product Not Found)
+ *          409:
+ *              description: Conflict Error (Already Have Comment In This Product)
+ */
+
+
+// ?----------------- Note Routes
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      createNote:
+ *          type: object
+ *          required:
+ *              -   text
+ *          properties:
+ *              text:
+ *                  type: string
+ *                  example: "its perfect"
  * 
+ *      updateNote:
+ *          type: object
+ *          required:
+ *              -   text
+ *          properties:
+ *              text:
+ *                  type: string
+ *                  example: "its perfect"
+ */
+
+/**
+ * @swagger
+ * /user/note/create/{productId}:
+ *  post:
+ *      summary: Create Note With (productId, Text)
+ *      tags:
+ *          -   User
+ *      parameters:
+ *          -   in: path        
+ *              name: productId
+ *              type: string
+ *      requestBody:
+ *          content:
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      $ref: "#/components/schemas/createNote"
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/createNote"
+ *      responses:
+ *          201:
+ *              description: Success
+ *          401:
+ *              description: Unauthorized Error (Authentication Error)
+ *          404:
+ *              description: NotFound Error (Product Not Found)
+ *          409:
+ *              description: Conflict Error (Already Have Note For This Product)
+ * 
+ * /user/note/update/{productId}:
+ *  post:
+ *      summary: Update Note With (productId, Text)
+ *      tags:
+ *          -   User
+ *      parameters:
+ *          -   in: path        
+ *              name: productId
+ *              type: string
+ *      requestBody:
+ *          content:
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      $ref: "#/components/schemas/updateNote"
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/updateNote"
+ *      responses:
+ *          201:
+ *              description: Success
+ *          401:
+ *              description: Unauthorized Error (Authentication Error)
+ *          404:
+ *              description: NotFound Error (Product Not Found Or Note Not Found)
+ * 
+ * /user/note/delete/{productId}:
+ *  post:
+ *      summary: Delete Note With (productId)
+ *      tags:
+ *          -   User
+ *      parameters:
+ *          -   in: path        
+ *              name: productId
+ *              type: string
+ *      responses:
+ *          201:
+ *              description: Success
+ *          401:
+ *              description: Unauthorized Error (Authentication Error)
+ *          404:
+ *              description: NotFound Error (Product Not Found Or Note Not Found)
+ * 
+ * /user/note/{productId}:
+ *  get:
+ *      summary: Get Note With (productId)
+ *      tags:
+ *          -   User
+ *      parameters:
+ *          -   in: path        
+ *              name: productId
+ *              type: string
+ *      responses:
+ *          200:
+ *              description: Success
+ *          401:
+ *              description: Unauthorized Error (Authentication Error)
+ *          404:
+ *              description: NotFound Error (Product Not Found Or Note Not Found)
+ * 
+ * /user/notes:
+ *  get:
+ *      summary: Get Notes
+ *      tags:
+ *          -   User
+ *      responses:
+ *          200:
+ *              description: Success
+ *          401:
+ *              description: Unauthorized Error (Authentication Error)
  */
